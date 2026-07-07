6fe1288c2ceda72c1cead437fde6a19596366439
Aml Abdelrhman <Aml586651@gmail.com>
2026-07-07 20:30:01 +0300

    Update database password config

--- config/database.php
+++ config/database.php
@@ -44,25 +44,20 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+     'mysql' => [
+    'driver' => 'mysql',
+    'host' => 'mysql.railway.internal', // هذا هو العنوان الثابت في Railway
+    'port' => '3306',
+    'database' => 'railway', // اسم قاعدة البيانات الافتراضي في Railway
+    'username' => 'root',   // اسم المستخدم الافتراضي
+   'password' => env('MYSQLPASSWORD'),
+    'unix_socket' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => true,
+    'engine' => null,
+],
 
         'mariadb' => [
             'driver' => 'mariadb',
